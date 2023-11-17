62ffe823c1b020c6ba0f5130d3755b124e6424ea
虞晨 <chen.yu@cicc.com.cn>
2023-11-17 13:51:53 +0800

    增加首页

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+// webpack的入口文件：src/index.tsx
 
-const App = () => {
-  return <div>66</div>;
-};
-export default App;
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Webpack V5 + React </h1>
+      </header>
+    </div>
+  );
+}
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
