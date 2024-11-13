1f7677a0c047039bbeecc2bf2a8934dcd305aaf2
虞晨 <chen.yu@cicc.com.cn>
2024-11-13 09:29:11 +0800

    增加fileloader,但是fileloader没有生效,文件路径多了一个斜杠

--- loader/prefix/index.js
+++ loader/prefix/index.js
@@ -3,6 +3,7 @@ console.log(schema, 'schema');
 module.exports = function format(content) {
 	// 获取webpack中options的配置
 	const options = this.getOptions(schema);
+
 	const { author } = options;
 	const prefix = `/* author: ${author} */`;
 	return prefix + content;
